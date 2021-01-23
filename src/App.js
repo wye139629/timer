752511d4d8f33e37a5639ac00fb11e92a781f8ce
will <wye139629@gmail.com>
2021-01-23 21:11:21 +0800

    set mins

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Background from "./component/ReactBackGround";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer></Timer>
     </div>
   );
 }
 
+const Timer = () => {
+  let [min, setMin] = useState(0);
+
+  return (
+    <div>
+      {/* <button onClick={plusHandler}>+</button> */}
+      <Button plus={true} min={min} setMin={setMin} />
+      <h1>{min}Minutes</h1>
+      <Button plus={false} min={min} setMin={setMin} />
+    </div>
+  );
+};
+
+function Button({ plus, min, setMin }) {
+  // console.log(props);
+  // const { plus } = props;
+  function clickHanlder() {
+    if (plus) {
+      // min += 1;
+      setMin(++min);
+      return;
+    }
+    if (min > 0) {
+      min -= 1;
+      setMin(min);
+    }
+  }
+  return <button onClick={clickHanlder}>{plus ? "+" : "-"}</button>;
+}
 export default App;
